4e66965e6ca069a4a52e56d6850081f79f3b17a8
Gem Nieva <mhigenieva@gmail.com>
2026-06-10 21:33:43 +0800

    test room 1(better quality)

--- server/server.js
+++ server/server.js
@@ -91,7 +91,10 @@ wss.on("connection", (ws, req) => {
           room1Receiver.readyState === WebSocket.OPEN
         ) {
 
-
+          console.log(
+            "Forwarding Nurse -> Room1",
+            data.length
+          );
 
           room1Receiver.send(data);
         }
@@ -107,6 +110,10 @@ wss.on("connection", (ws, req) => {
         nurseReceiver.readyState ===
         WebSocket.OPEN
       ) {
+        console.log(
+          "Forwarding Room1 -> Nurse",
+          data.length
+        );
 
         nurseReceiver.send(data);
       }
